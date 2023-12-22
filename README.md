b288da7a742d2e329440b0855f83454e4879f9b9
Shane Curcuru <asf@shanecurcuru.org>
2023-12-22 10:59:37 -0500

    Preliminary functionality

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# propublica990
-Tools for ProPublica's Nonprofit Explorer 990 tax forms API v2.
+# Propublica990 Ruby utility modules
+
+Ruby utility modules using Propublica's V2 API to get basic 990 financials easily.
+
+## Timeline of 990 data publication by IRS
+
+The IRS' publication of 990 data forms has changed over the years, sometimes significantly.  Many older 990 code projects were designed for either earlier data structures or ways to get the 990 data (from AWS, from IRS, or elsewhere).  Caution is advised when evaluating code!
+
+## More useful IRS 990 analysis tools
+
+[IRSx](http://www.irsx.info/) is a [comprehensive Python toolkit and database](https://github.com/jsfenfen/990-xml-reader/) for grabbing, parsing, and manipulating the full 990 XML structures.
+
+Nonprofit Open Data Collective's [irs990efile](https://github.com/Nonprofit-Open-Data-Collective/irs990efile) R project includes a great [data dictionary mapping fields](https://nonprofit-open-data-collective.github.io/irs990efile/data-dictionary/data-dictionary.html) across 990, EZ, and PF forms. A prior version was called [irs-990-efiler-database](https://github.com/Nonprofit-Open-Data-Collective/irs-990-efiler-database).
+
+Open Data Collective also has an [overview of other 990 resources](https://github.com/Nonprofit-Open-Data-Collective/irs-990-data-issue-tracker), and some [R tools for compensation and director/officer name scanning](https://github.com/Nonprofit-Open-Data-Collective/irs-990-compensation-data).
+
+TechByOrg's [irs-990-api](https://github.com/techbyorg/irs-990-api) uses GraphQL to build and analyze databases of 990 data.
+
+[CharityNavigator's (archived) 990_long](https://github.com/CharityNavigator/990_long) R/python code and analysis of 990 forms was built on top of the [(obsolete) irs_990 Python/SQL tools](https://github.com/CharityNavigator/irs990).
+
+For 990PF Private Foundation tools, [Grantmakers](https://www.grantmakers.io/) has code and databases to help.
+
+For ad hoc research, Guidestar and Foundation Center have merged into [Candid](https://beta.candid.org/), a more comprehensive nonprofit search website.
--- propublica990.rb
+++ propublica990.rb
@@ -1,53 +1,142 @@
 #!/usr/bin/env ruby
-#
 module Propublica990
   DESCRIPTION = <<-HEREDOC
   Propublica990: Fetch/parse IRS 990 data from ProPublica API v2:
     https://projects.propublica.org/nonprofits/api
     Utilities used for fossfoundation.info and fossfunding.com
-    See also: https://github.com/ShaneCurcuru/irs990
+    See also deprecated: https://github.com/ShaneCurcuru/irs990
   HEREDOC
   module_function
   require 'json'
+  require 'csv'
   require 'open-uri'
+  require './fieldmap990'
 
   # Constants related to ProPublica API v2
   PROPUBLICA_APIV2 = 'https://projects.propublica.org/nonprofits/api/v2/organizations/'
   PROPUBLICA_APIV2_JSON = '.json'
+  ORGANIZATION = 'organization'
+  FILINGS = 'filings_with_data'
 
-  # Fetch an org's data from ProPublica
+  # Newspaper nonprofits to analyze in New England
+  LOCAL_NEWS = %w[871248884	460777549	237246801	874640985	843780597	862407296	834616910	882367192	863807140	920697644	882058638]
+  # FOSS Foundation nonprofits: https://github.com/Punderthings/fossfoundation/tree/main/_foundations taxID field (when is an EIN)
+
+  # Fetch an org's data from ProPublica as json hash
   def fetch_org(ein)
-    org = JSON.load_file(URI.open(PROPUBLICA_APIV2 + ein + PROPUBLICA_APIV2_JSON))
-    return org
+    return JSON.load(URI.open(PROPUBLICA_APIV2 + ein + PROPUBLICA_APIV2_JSON))
   end
 
-  # Cache org's data in local directory
+  # Cache org's data in local file
   # @return true if we updated the file because any newer filing data found
-  def cache_org(ein, dir)
+  def cache_org(ein, file)
     org = fetch_org(ein)
-    fn = File.join(dir, "#{ein}.json")
-
     newerdata = true
-    if File.exist?(fn)
-      cache = JSON.load_file(fn)
+    if File.exist?(file)
+      cache = JSON.load_file(file)
       newdate = org["filings_with_data"].map { |i| DateTime.parse(i["updated"]) }.max
-      cachedate = cache["filings_with_data"].map { |i| DateTime.parse(i["updated"]) }.max
-      newerdata = newdate > cachedate
+      if newdate.nil?
+        newerdata = false # FIXME validate what we want here; maybe check vs. org.updated_at?
+      else
+        cachedate = cache["filings_with_data"].map { |i| DateTime.parse(i["updated"]) }.max
+        newerdata = newdate > cachedate unless cachedate.nil?
+      end
     end
     if newerdata
-      File.write(File.join(dir, "#{ein}.json"), JSON.pretty_generate(org))
-      return true
-    else
-      return false
+      File.write(file, JSON.pretty_generate(org))
+    end
+    return newerdata
+  end
+
+  # Get an org's data
+  # @param ein as string of entire EIN without dash
+  # @param dir as local directory to cache "#{ein}.json" files into
+  # @param refresh if true, force a lookup from Propublica for any newer data
+  # @return hash of Propublica Organization object
+  def get_org(ein, dir, refresh = false)
+    file = File.join(dir, "#{ein}.json")
+    if refresh or !File.exist?(file)
+      unused = cache_org(ein, file)
+    end
+    return JSON.load_file(file)
+  end
+
+  # Get and cache an array of eins as orgs
+  # @return array of orgs (as hashes); any org with an error returns as a string
+  def get_orgs(eins, dir, refresh = false)
+    orgs = []
+    eins.each do |ein|
+      org = get_org(ein, dir, refresh)
+      if org.nil? || org.empty?
+        orgs << "ERROR: #{ein} returned nil or empty data"
+      else
+        orgs << org
+      end
+    end
+    return orgs
+  end
+
+  # Emit a simple string describing the org and recent filings available
+  # TODO Decide on minimal fields useful for comparisons
+  def report_org(ein, dir)
+    o = get_org(ein, dir)
+    org = o[ORGANIZATION]
+    report = {'name' => org['name']}
+    %w(state subsection_code ruling_date tax_period ntee_code classification_codes).each do |field|
+      report[field] = org[field]
+    end
+    return report
+  end
+
+  # Return flattened version of a single filing's fields
+  # @param filing single filings_with_data hash
+  # @param fields, fieldname mapping
+  # @return array of values from that filing
+  def flatten_filing(filing, fields)
+    flat = []
+    fields.each do |field,displayname|
+      flat << filing[field]
+    end
+    return flat
+  end
+
+  # Return flattened list of filings_with_data from an org
+  def flatten_filings(org, fields)
+    rows = []
+    filings = org[FILINGS]
+    filings.each do |filing|
+      rows << flatten_filing(filing, fields)
+    end
+    return rows
+  end
+
+  # Transform an array of orgs into a simple csv by ein,year across available data
+  def orgs2csv(orgs, fields, file)
+    rows = []
+    orgs.each do |org|
+      orgfilings = flatten_filings(org, fields)
+      orgfilings.each do |orgfiling|
+        rows << orgfiling
+      end
+    end
+    CSV.open(file, "w") do |csv|
+      csv << fields.values
+      rows.each do |r|
+        csv << r
+      end
     end
   end
 
-  # ### #### ##### ######
-  # Main method for command line use
-  if __FILE__ == $PROGRAM_NAME
-    path = File.join(Dir.pwd, '_data')
-    ein = '470825376'
-    puts "DEBUG: #{ein}, #{path}"
-    puts cache_org(ein, path)
-  end
+# ### #### ##### ######
+# Main method for command line use
+if __FILE__ == $PROGRAM_NAME
+  dir = File.join(Dir.pwd, '_data')
+
+  # ein = '874640985'
+  # org = Propublica990.get_org(ein, dir, true)
+  # puts Propublica990.report_org(ein, dir)
+
+  orgs = Propublica990.get_orgs(Propublica990::LOCAL_NEWS, dir)
+  csvfile = File.join(dir, "report-allnews.csv")
+  Propublica990.orgs2csv(orgs, FieldMap990::MAP_COMMON, csvfile)
 end
